876c20161bdee5b135cf592f9568956dddc6268d
hooman mousavi <hoomanmousavi77@gmail.com>
2023-06-12 18:13:17 +0330

    css dep added

---
+++ client/dist/main.fabe4906d156cdcdaf75.hot-update.js
@@ -0,0 +1,11 @@
+"use strict";
+self["webpackHotUpdatetemplate"]("main",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "e0c122a5e8221c1c37db"; }
+/******/ }();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=main.fabe4906d156cdcdaf75.hot-update.js.map
--- client/src/app.ts
+++ client/src/app.ts
@@ -1,4 +1,8 @@
-import './assets/style/app.scss';
+import './assets/app.scss';
+import 'materialize-css/dist/js/materialize.min';
+import ApexCharts from 'apexcharts'
 import Alpine from 'alpinejs'
 window.Alpine = Alpine
 Alpine.start()
+
+
